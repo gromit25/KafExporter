d261924b4c4f9ad9bce621641c43804de50bd3f1
jung mo <gromit25@hanmail.net>
2026-01-09 16:09:33 +0900

    Update ConsumerAdvice.java

--- src/main/java/com/redeye/kafexporter/acquisitor/advice/ConsumerAdvice.java
+++ src/main/java/com/redeye/kafexporter/acquisitor/advice/ConsumerAdvice.java
@@ -12,24 +12,21 @@ import net.bytebuddy.asm.Advice;
  * @author jmsohn
  */
 public class ConsumerAdvice {
-	
-	// !주의 어드바이스 메소드에서 static 멤버 변수를 참조할 때는 무조건 public 이어야함
-	// 같은 클래스 내에 있더라도 private 이면 bytebuddy에서 접근 오류를 발생함 - 이유는 확인 불가
+
+	private static BlockingQueue<> queue;
+
+	public static void init(BlockingQueue<> queue) {
+		this.queue = queue;
+	}
 	
 	/**
 	 * KafkaConsumer.poll 진입 전
+	 *
+	 * @param clientId 컨슈머 클라이언트 아이디
 	 */
 	@Advice.OnMethodEnter
-	public static void onEnter() {
+	public static void onEnter(@Advice.FieldValue("clientId") String clientId) {
 		long now = System.currentTimeMillis();
+		queue.put(now);
 	}
-
-	/**
-	 * KafkaConsumer.poll 반환 후
-	 * 
-	 * @param result poll 반환 값
-	 */
-	@Advice.OnMethodExit
-	public static void onExit(@Advice.Return ConsumerRecords<?, ?> result) {
-    }
 }
