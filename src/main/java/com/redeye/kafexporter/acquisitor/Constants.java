233749b203e71e8c6730e3b7cd1a331e41a9d52d
jung mo <gromit25@hanmail.net>
2026-01-15 17:19:02 +0900

    Update Constants.java

--- src/main/java/com/redeye/kafexporter/acquisitor/Constants.java
+++ src/main/java/com/redeye/kafexporter/acquisitor/Constants.java
@@ -7,6 +7,9 @@ package com.redeye.kafexporter.acquisitor;
  */
 public class Constants {
 	
-	/** 클라이언트 아이디 설정 키값 */
+	/** 클라이언트 아이디 설정 키 값 */
 	public static String CLIENT_ID = "client.id";
+	
+	/** 디폴트 클라이언트 아이디 값 */
+	public static String DEFAULT_CLIENT_ID = "N/A";
 }
