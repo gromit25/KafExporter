3253ac9ff51a5d8ee9993b0ac028443a6a5b5260
jung mo <gromit25@hanmail.net>
2026-01-15 17:19:36 +0900

    Update KafkaAcquisitor.java

--- src/main/java/com/redeye/kafexporter/acquisitor/KafkaAcquisitor.java
+++ src/main/java/com/redeye/kafexporter/acquisitor/KafkaAcquisitor.java
@@ -9,6 +9,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 import com.redeye.kafexporter.util.StringUtil;
 import com.redeye.kafexporter.acquisitor.advice.ConsumerConfigAdvice;
 import com.redeye.kafexporter.acquisitor.advice.KafkaConsumerAdvice;
+import com.redeye.kafexporter.acquisitor.advice.KafkaConsumerConstructorAdvice;
 import com.redeye.kafexporter.acquisitor.advice.ProducerConfigAdvice;
 import com.redeye.kafexporter.acquisitor.jmx.JMXService;
 import com.redeye.kafexporter.acquisitor.model.IntervalDTO;
@@ -76,8 +77,8 @@ public class KafkaAcquisitor {
 			throw new IllegalArgumentException("'inst' is null.");
 		}
 		
-		// 메소드 훅킹 인터셉터 등록
-		this.addTransformer(inst);
+		// Kafka 메소드 인터셉터 등록
+		this.addKafkaTransformer(inst);
 		
 		//
 		this.intervalConsumerDaemon = new QueueDaemon<>(
@@ -93,26 +94,14 @@ public class KafkaAcquisitor {
 	}
 	
 	/**
-	 * 메소드 훅킹 인터셉터 등록 - 바이트 코드 변환
+	 * 메소드 인터셉터 등록 - 바이트 코드 변환
 	 * 
 	 * @param inst Java 인스트루먼트 객체
 	 */
-	private void addTransformer(Instrumentation inst) {
+	private void addKafkaTransformer(Instrumentation inst) {
 		
-		// KafkaConsumer의 poll 메소드 호출 어드바이스 설정
-		KafkaConsumerAdvice.init(intervalQueue);
+		// --- Kafka ProducerConfig 메소드 훅킹 설정
 		
-		new AgentBuilder.Default()
-			.type(ElementMatchers.named("org.apache.kafka.clients.consumer.KafkaConsumer"))
-			.transform(
-				(builder, typeDescription, classLoader, module, protectedDomain) -> {
-					return builder
-						.method(ElementMatchers.named("poll"))
-						.intercept(Advice.to(KafkaConsumerAdvice.class));
-				}
-			)
-	    	.installOn(inst);
-
 		// Kafka ProducerConfig 생성자 호출 어드바이스 설정
 		ProducerConfigAdvice.init(producerConfigMap);
 			
@@ -127,6 +116,8 @@ public class KafkaAcquisitor {
 			)
         	.installOn(inst);
 		
+		// --- Kafka ConsumerConfig 메소드 훅킹 설정
+		
 		// Kafka ConsumerConfig 생성자 호출 어드바이스 설정
 		ConsumerConfigAdvice.init(consumerConfigMap);
 		
@@ -139,7 +130,50 @@ public class KafkaAcquisitor {
 						.intercept(Advice.to(ConsumerConfigAdvice.class));
 				}
 			)
-        	.installOn(inst);
+			.installOn(inst);
+		
+		// --- KafkaConsumer 메소드 훅킹 설정
+
+		// KafkaConsumer의 생성자 호출 어드바이스 설정
+		new AgentBuilder.Default()
+			.type(ElementMatchers.named("org.apache.kafka.clients.consumer.KafkaConsumer"))
+			.transform(
+				(builder, typeDescription, classLoader, module, protectionDomain) -> { 
+					return builder
+						.constructor(ElementMatchers.any())
+						.intercept(Advice.to(KafkaConsumerConstructorAdvice.class));
+				}
+			)
+			.installOn(inst);
+		
+		// 스프링부트의 KafkaConsumer의 생성자 호출 어드바이스 설정
+		new AgentBuilder.Default()
+			.type(ElementMatchers.named("org.springframework.kafka.core.DefaultKafkaConsumerFactory$ExtendedKafkaConsumer"))
+			.transform(
+				(builder, typeDescription, classLoader, module, protectionDomain) -> { 
+					return builder
+						.constructor(ElementMatchers.any())
+						.intercept(Advice.to(KafkaConsumerConstructorAdvice.class));
+				}
+			)
+			.installOn(inst);
+		
+		// KafkaConsumer의 poll 메소드 호출 어드바이스 설정
+		KafkaConsumerAdvice.init(intervalQueue);
+		
+		new AgentBuilder.Default()
+			.type(ElementMatchers.named("org.apache.kafka.clients.consumer.KafkaConsumer"))
+			.transform(
+				(builder, typeDescription, classLoader, module, protectedDomain) -> {
+					return builder.visit(
+						Advice
+							.to(KafkaConsumerAdvice.class)
+							.on(ElementMatchers.named("poll")
+							.and(ElementMatchers.takesArguments(1)))
+					);
+				}
+			)
+	    	.installOn(inst);
 	}
 	
 	/**
