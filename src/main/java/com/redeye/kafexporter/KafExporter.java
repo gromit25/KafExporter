3da4a8b4ab09aaac751dfb5d85e0d91ef56e33ce
jung mo <gromit25@hanmail.net>
2026-01-15 17:16:45 +0900

    Create KafExporter.java

---
+++ src/main/java/com/redeye/kafexporter/KafExporter.java
@@ -0,0 +1,34 @@
+package com.redeye.kafexporter;
+
+import java.lang.instrument.Instrumentation;
+
+import com.redeye.kafexporter.acquisitor.KafkaAcquisitor;
+import com.redeye.kafexporter.http.HttpExporter;
+
+/**
+ * 
+ * 
+ * @author jmsohn
+ */
+public class KafExporter {
+	
+	/**
+	 * 
+	 * 
+	 * @param args
+	 * @param inst
+	 */
+	public static void premain(String args, Instrumentation inst) {
+		
+		KafkaAcquisitor.getInstance().init(inst);
+		
+		try {
+			
+			HttpExporter server = new HttpExporter(5551);
+			server.start();
+			
+		} catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
