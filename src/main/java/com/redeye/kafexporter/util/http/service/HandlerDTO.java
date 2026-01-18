9195e47dae739b80593d08227d80e15c2e5c08ff
gromit25 <gromit25@hanmail.net>
2026-01-18 20:49:51 +0900

    메소드 타입 추가

--- src/main/java/com/redeye/kafexporter/util/http/service/HandlerDTO.java
+++ src/main/java/com/redeye/kafexporter/util/http/service/HandlerDTO.java
@@ -1,6 +1,7 @@
 package com.redeye.kafexporter.util.http.service;
 
 import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -19,6 +20,15 @@ import lombok.Getter;
 class HandlerDTO {
 	
 	
+	/**
+	 * 
+	 */
+	private enum MethodType {
+		ONE_PARAM,
+		TWO_PARAM;
+	}
+	
+	
 	/** */
 	@Getter
 	private final StringUtil.WildcardPattern pathPattern;
@@ -26,6 +36,9 @@ class HandlerDTO {
 	/** */
 	@Getter
 	private final Set<HttpMethod> httpMethodSet;
+	
+	/** */
+	private final MethodType methodType;
 	
 	/** */
 	@Getter
@@ -41,18 +54,64 @@ class HandlerDTO {
 	 */
 	public HandlerDTO(String path, HttpMethod[] httpMethodList, Method method) throws Exception {
 		
-		//
+		// 1. Path 설정
 		this.pathPattern = StringUtil.WildcardPattern.create(path);
 		
-		//
+		// 2. HTTP 메소드 설정
 		this.httpMethodSet = new HashSet<>();
 		for(HttpMethod httpMethod: httpMethodList) {
 			this.httpMethodSet.add(httpMethod);
 		}
 		
-		//
+		// 3. 메소드 타입 및 메소드 설정
+		this.methodType = getMethodType(method);
 		this.method = method;
 	}
+	
+	/**
+	 * 
+	 * 
+	 * @param method
+	 * @return
+	 */
+	private static MethodType getMethodType(Method method) {
+		
+		// 1. public 여부 확인
+		if(Modifier.isPublic(method.getModifiers()) == false) {
+			throw new RuntimeException(method +  " is not public.");
+		}
+
+		// 2. 반환 타입이 String인지 확인
+		if(method.getReturnType().equals(String.class) == false) {
+			throw new RuntimeException(method +  " return type must be String.");
+		}
+
+		// 3. 파라미터 개수 및 타입 확인
+		MethodType methodType = null;
+		
+		Class<?>[] parameterTypes = method.getParameterTypes();
+		if(parameterTypes.length == 1) {
+			methodType = MethodType.ONE_PARAM;
+		} else if(parameterTypes.length == 2) {
+			methodType = MethodType.TWO_PARAM;
+		} else {
+			throw new RuntimeException(method +  " must have 1 or 2 params.");
+		}
+
+		// 첫 번째 파라미터: HttpExchange
+		if(parameterTypes[0].equals(HttpExchange.class) == false) {
+			throw new RuntimeException(method +  " first param must be HttpExchange.");
+		}
+
+		// 두 번째 파라미터: List
+		if(methodType == MethodType.TWO_PARAM) {
+			if(parameterTypes[1].equals(List.class) == false) {
+				throw new RuntimeException(method +  " second param must be List.");
+			}
+		}
+
+		return methodType;
+    }
 
 	/**
 	 * 
@@ -97,11 +156,19 @@ class HandlerDTO {
 	public String invoke(Object obj, HttpExchange exchange) throws Exception {
 		
 		//
-		String path = exchange.getRequestURI().getPath();
-		List<String> params = this.pathPattern.match(path).getGroups();
+		Object retrival = null;
 		
-		//
-		Object retrival = this.method.invoke(obj, exchange);
+		if(this.methodType == MethodType.ONE_PARAM) {
+			
+			retrival = this.method.invoke(obj, exchange);
+			
+		} else if(this.methodType == MethodType.TWO_PARAM) {
+			
+			String path = exchange.getRequestURI().getPath();
+			List<String> params = this.pathPattern.match(path).getGroups();
+			
+			retrival = this.method.invoke(obj, exchange, params);
+		}
 		
 		//
 		return (retrival != null)?retrival.toString():"";
