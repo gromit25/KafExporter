b8696cece15d3053729b1c44ec4144c8f3ca0b19
jung mo <gromit25@hanmail.net>
2026-01-22 15:53:23 +0900

    Create KafkaConsumerCommitSyncAdvice.java

---
+++ src/main/java/com/redeye/kafexporter/acquisitor/kafka/advice/KafkaConsumerCommitSyncAdvice.java
@@ -0,0 +1,25 @@
+package com.redeye.kafexporter.acquisitor.kafka.advice;
+
+import net.bytebuddy.asm.Advice;
+
+/**
+ * KafkaConsumer commitAsync 어드바이스 클래스
+ * 
+ * @author jmsohn
+ */
+public class KafkaConsumerCommitSyncAdvice {
+	
+	@Advice.OnMethodExit
+	public static void onExit(@Advice.This Object consumer) {
+		
+		System.out.println("### INVOKE commitSync DEBUG 000");
+		
+		// 클라이언트 아이디 획득
+		String clientId = KafkaConsumerConstructorAdvice.getClientId(consumer);
+		if(clientId == null) {
+			return;
+		}
+		
+		System.out.println("### INVOKE commitSync DEBUG 100: " + clientId);
+	}
+}
