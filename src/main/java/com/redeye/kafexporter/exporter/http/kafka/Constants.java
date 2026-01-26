1654b2403265bf5dac2a9b7ef4ab747e1b2f7aa5
jung mo <gromit25@hanmail.net>
2026-01-26 17:34:28 +0900

    Create Constants.java

---
+++ src/main/java/com/redeye/kafexporter/exporter/http/kafka/Constants.java
@@ -0,0 +1,18 @@
+package com.redeye.kafexporter.exporter.http.kafka;
+
+/**
+ * 상수 클래스
+ * 
+ * @author jmsohn
+ */
+public class Constants {
+	
+	/** 브로커 패스 세그먼트 */
+	public static final String PATH_BROKER = "broker";
+	
+	/** 프로듀서 패스 세그먼트 */
+	public static final String PATH_PRODUCER = "producer";
+	
+	/** 컨슈머 패스 세그먼트 */
+	public static final String PATH_CONSUMER = "consumer";
+}
